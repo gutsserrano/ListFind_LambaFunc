c8c2127669b45e88fc270fb4fec92489ecb68dae
gutsserrano <gutsserrano@gmail.com>
2024-05-08 11:14:02 -0300

    Add project files.

---
+++ List_Find/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Find
+{
+    internal class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Pessoa(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
---
+++ List_Find/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace List_Find
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pessoa> list = new List<Pessoa>();
+
+            list.Add(new Pessoa(1, "Jose"));
+            list.Add(new Pessoa(3, "Fabio"));
+            list.Add(new Pessoa(2, "Mario"));
+
+            if(list.Exists((p => p.Id == 3)))
+            {
+                Console.WriteLine("Achou 1!");
+            }
+            else
+            {
+                Console.WriteLine("Não achou!");
+            }
+
+            string aux = "Jose";
+            foreach(Pessoa p in list)
+            {
+                if(p.Nome == aux)
+                {
+                    Console.WriteLine("Achou 2!");
+                }
+            }
+        }
+    }
+}
